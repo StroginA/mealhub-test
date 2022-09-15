23efde27d3cfd78d0f4db7d62b91417a40a719d9
StroginA <103519095+StroginA@users.noreply.github.com>
2022-09-15 15:25:56 +0300

    keep-alive for pagination

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="app">
+        <Nuxt keep-alive class="main-view" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'default',
+}
+</script>
+
+<style scoped>
+    .app {
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+    }
+</style>
